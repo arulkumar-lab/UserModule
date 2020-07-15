e92c70e086814b99f403765157e172c0c88000d9
radhakar <Jan2019$$>
2020-07-15 10:50:41 +0100

    initial

---
+++ service-a/src/main/java/com/arului/UserModule/UserModuleRestApplication.java
@@ -0,0 +1,11 @@
+package com.arului.UserModule;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath ("/data")
+public class UserModuleRestApplication extends Application {
+}
---
+++ service-b/src/main/java/com/arului/UserModule/UserModuleRestApplication.java
@@ -0,0 +1,12 @@
+package com.arului.UserModule;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath ("/data")
+
+public class UserModuleRestApplication extends Application {
+}
